242525b257794a55ef7d877cc6d7fa63521f741e
DrVicki <vbealman@fullsail.com>
2021-04-23 15:37:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-xbvwaw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-xbvwaw)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Cloudflare!</h1>
+      <p>Deploy React App to Cloudflare</p>
+    </div>
+  );
+}
